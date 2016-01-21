dcb0a4997172231d9bf1b78aa733ac20063add5c
Sumeet Padavala <sumeet.padavala@gmail.com>
2016-01-21 15:45:41 +0000

    reset everything, switch to Blaze and Materializecss, got some basic data working

---
+++ flux/flux.js
@@ -0,0 +1,24 @@
+/*global groups,events*/
+groups = new Mongo.Collection("groups");
+events = new Mongo.Collection("events");
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.groupPanel.helpers({
+    groupData: function () {
+      return groups.find().fetch();
+    }
+  });
+  Template.eventsPanel.helpers({
+    eventsData: function () {
+      return events.find().fetch();
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
